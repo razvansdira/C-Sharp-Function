f76b0cb79a845d17ca77ee9a0545af57f183baf0
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-29 10:41:31 +0200

    Add files via upload

---
+++ Exercise1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Function
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            welcome();
+            nice();
+            Console.ReadLine();
+        }
+
+        static void welcome()
+        {
+            Console.WriteLine("   Welcome !!!!");
+        }
+
+        static void nice()
+        {
+            Console.Write(" Have a nice day.");
+        }
+    }
+}
