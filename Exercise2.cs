6f5b35e4b5b2dd6aabb7d87913efb0d92bf0b84e
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-29 10:38:25 +0200

    Add files via upload

---
+++ Exercise2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Function
+{
+    class Exercise2
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.Write(" Input a name: ");
+            a = Console.ReadLine();
+            welcome(a);
+            nice();
+            Console.ReadLine();
+        }
+
+        static void welcome(string n)
+        {
+            Console.WriteLine("  Welcome friend " + n);
+        }
+
+        static void nice()
+        {
+            Console.WriteLine(" Have a nice day!!!");
+        }
+    }
+}
