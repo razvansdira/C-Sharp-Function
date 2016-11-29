378df1ffd2436bcaa918f2519c52ea3b329be29f
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-29 12:13:29 +0200

    Add files via upload

---
+++ Exercise4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Function
+{
+    class Exercise4
+    {
+        static int space(string a)
+        {
+            string b;
+            int ctr=0;
+            for(int i=0; i<a.Length; i++)
+            {
+                b = a.Substring(i, 1);
+                if (b == " ")
+                    ctr++;
+            }
+            return ctr++;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("  Input a string: ");
+            string c = Console.ReadLine();
+            Console.WriteLine(c+" -> contains {0} spaces.", space(c));
+            Console.ReadLine();
+        }
+
+    }
+}
