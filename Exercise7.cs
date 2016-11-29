9b71488ef7c49c902a8e2d4ca99def670819985c
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-29 12:47:32 +0200

    Add files via upload

---
+++ Exercise7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Function
+{
+    class Exercise7
+    {
+        static int pow(int a, int b)
+        {
+            int i, x=1;
+            for (i = 1; i <= b; i++)
+            x = x* a;
+            return x;
+        }
+
+        static void Main(string[] args)
+        {
+            int m, n;
+            Console.Write("  Input the base: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" Input the exponent: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("  The result is: {0}", pow(m,n));
+            Console.ReadLine();
+        }
+    }
+}
