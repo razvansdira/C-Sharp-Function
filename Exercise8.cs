c1a428eba9e33b1858cc5266a5d2d5170fc0dd5e
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-29 13:03:56 +0200

    Add files via upload

---
+++ Exercise8.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Function
+{
+    class Exercise8
+    {
+        static int fib(int n)
+        {
+            int i, m, a = 0, b = 1;
+            for (i = 0; i < n; i++)
+            {
+                m = a;a = b;b += m;
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.Write("  Input the size: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" The Fibonacci series is: ");
+            for (y = 0; y < x; y++)
+                Console.Write(fib(y) + " ");
+            Console.ReadLine();
+        }
+    }
+}
