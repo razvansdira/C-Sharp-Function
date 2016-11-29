1aa08ed108c6455d7ea3d88bc83a6f0b517e30ec
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-29 13:16:48 +0200

    Add files via upload

---
+++ Exercise10.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Function
+{
+    class Exercise10
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write(" Input a big number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("   The sum is {0}.", tot(n));
+            Console.ReadLine();
+        }
+
+        static int tot(int x)
+        {
+            string a = Convert.ToString(x);
+            int sum = 0;
+
+            for (int i = 0; i < a.Length; i++)
+                sum += Convert.ToInt32(a.Substring(i, 1));
+            return sum;
+        }
+    }
+}
